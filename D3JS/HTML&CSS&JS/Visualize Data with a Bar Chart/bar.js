dd32b29bcc57c4f24f10a7ccd88d7b7b343a3d77
Eoin Carrick <22carri99@gmail.com>
2022-02-17 19:59:40 +0000

    code

--- D3JS/HTML&CSS&JS/Visualize Data with a Bar Chart/bar.js
+++ D3JS/HTML&CSS&JS/Visualize Data with a Bar Chart/bar.js
@@ -1,14 +1,57 @@
 const barChart = async () => {
+  //Getting data and creating our x and y Accessor function.
   const data = await d3.json(
     "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
   );
-  console.table(data.data);
-  const dataParse = d3.timeParse("%Y-%m-%d");
-  const xAccessor = (d) => dataParse(d.data[0]);
-  const yAccessor = (d) => d.data[1];
-  console.log(xAccessor)
+  dataset = data;
+  console.log();
+  const yAccessor = (d) => d.data[1][1];
+  const dateParse = d3.timeParse("%Y-%m-%d");
+  const xAccessor = (d) => dateParse(d.data[1][0]);
+  console.log(xAccessor);
 
+  //Drawing and setting our dimensions.
+  let dimensions = {
+    width: window.innerWidth * 0.8,
+    height: window.innerHeight * 0.5,
+    margin: {
+      top: 10,
+      right: 10,
+      left: 60,
+      bottom: 50,
+    },
+  };
 
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.right - dimensions.margin.left;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const g = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  //Setting ou scale.
+
+  //   yScale = d3
+  //     .scaleLinear()
+  //     .domain(d3.extent(data, yAccessor))
+  //     .range([dimensions.boundedHeight, 0]);
+  //   console.log(yScale(32));
+
+  //   xScale = d3
+  //     .scaleTime()
+  //     .domain(d3.extent(data, xAccessor))
+  //     .range([0, dimensions.boundedWidth]);
+  //   console.log(xScale(23))
 };
 barChart();
